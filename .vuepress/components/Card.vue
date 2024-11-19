97d61c3bbc18b8c6d8ff70d61267e6d831759074
spring <365246692.@qq.com>
2024-11-19 10:04:24 +0800

    样式更新, 名人堂给你也加上

--- .vuepress/components/Card.vue
+++ .vuepress/components/Card.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="user-grid">
-    <div v-for="user in users" :key="user.uid" class="user-card">
+    <div v-for="user in users" :key="user.uid" :class="getCardClass(user)">
       <a :href="user.url" target="_blank" rel="noopener noreferrer">
         <img
           :src="`https://a.ppy.sh/${user.uid}`"
@@ -19,6 +19,7 @@
 
 <script setup lang="ts">
 interface User {
+  disable?: boolean;
   uid: string;
   name: string;
   url: string;
@@ -33,9 +34,19 @@ const handleImageError = (e: Event) => {
   const img = e.target as HTMLImageElement;
   img.src = "/placeholder-avatar.png";
 };
+
+const getCardClass = (user) => {
+  if (user.disable) {
+    return "user-card disable";
+  } else if (user.stuff) {
+    return "user-card stuff";
+  } else {
+    return "user-card";
+  }
+}
 </script>
 
-<style scoped>
+<style scoped lang="scss">
 .user-grid {
   display: grid;
   grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
@@ -50,6 +61,47 @@ const handleImageError = (e: Event) => {
   border-radius: 0.5rem;
   background: var(--c-bg);
   transition: transform 0.2s;
+}
+
+.disable {
+  &:hover {
+    transform: none !important;
+  }
+  &::before {
+    content: "";
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    z-index: 1;
+    pointer-events: all;
+    border-radius: 1rem;
+    background-color: rgba(0, 0, 0, 0.3);
+    cursor: not-allowed;
+  }
+
+  filter: grayscale(100%);
+}
+
+.stuff {
+  position: relative;
+  &::after {
+    content: "";
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    z-index: -1;
+    border-radius: 0.5rem;
+    opacity: 0;
+    background-image: linear-gradient(to top, #f3e7e9 0%, #e3eeff 99%, #e3eeff 100%);
+    transition: opacity 0.2s;
+  }
+  &:hover::after {
+    opacity: 1;
+  }
 }
 
 .user-card:hover {
--- people/alumni.md
+++ people/alumni.md
@@ -30,22 +30,32 @@ import Card from '@/Card.vue'
 
 const contributors = [
   {
+    stuff: true,
+    uid: "35628968",
+    name: "SisypheOvO",
+    url: "https://osu.ppy.sh/users/35628968",
+    contribution: "调整网站样式"
+  },
+  {
+    stuff: true,
     uid: "13552636",
     name: "- E C -",
     url: "https://osu.ppy.sh/users/13552636",
     contribution: "制作集锦"
   },
   {
+    stuff: true,
     uid: "17268434",
     name: "PercyDan",
     url: "https://osu.ppy.sh/users/17268434",
     contribution: "制作集锦"
   },
   {
+    disable: true,
     uid: "19755783",
     name: "Vup",
     url: "https://osu.ppy.sh/users/19755783",
-    contribution: "作为猫猫机器人的代行账号"
+    contribution: "曾作为qq bot\n现在账号已封禁"
   }
 ];
 </script>
